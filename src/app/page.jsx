1dee5581c7b8062290d1a581534f4d0315a0fea6
Maciej Gierałtowski <kolorowy517@gmail.com>
2023-11-19 18:01:50 +0100

    defined global css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,33 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <Link href='/'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+  Corrupti exercitationem rerum repellendus, numquam magnam voluptatem voluptatibus 
+  dolore vero quae consequatur? Ipsum laudantium distinctio porro. 
+  Iste laboriosam aperiam quos rerum aspernatur.</Link>;
 }
